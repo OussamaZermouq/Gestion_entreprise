7cfc6226b5d780f1bdb3718f406010ea782d3b49
OussamaZermouq <oussamazermouq2@gmail.com>
2023-12-28 18:19:38 +0100

    Working on the internalframe

--- src/main/java/org/example/Interfaces/InternalFrameDemo.java
+++ src/main/java/org/example/Interfaces/InternalFrameDemo.java
@@ -1,35 +1,70 @@
 package org.example.Interfaces;
-import javax.swing.JInternalFrame;
-import javax.swing.JDesktopPane;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.JMenuBar;
-import javax.swing.JFrame;
-import javax.swing.KeyStroke;
+import javax.swing.*;
 
 import java.awt.event.*;
 import java.awt.*;
+import java.sql.SQLException;
 
 /*
  * InternalFrameDemo.java requires:
  *   InternalFrameDemo.java
  */
 public class InternalFrameDemo extends JFrame{
-    public static void main(String[] args) {
-        JFrame jf=new JFrame();
-        jf.setLayout(null);
-        jf.setSize(1280, 720);
-        jf.setVisible(true);
-        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    private JDesktopPane desktopPane;
 
-        JInternalFrame jInternalFrame=new JInternalFrame();
-        jInternalFrame.setLocation(100, 100);
-        jInternalFrame.setSize(500, 300);
-        jInternalFrame.setTitle("Internal frame");
-        jInternalFrame.setVisible(true);
-        jInternalFrame.setClosable(true);
-        jInternalFrame.setResizable(true);
-        jf.add(jInternalFrame);
-        jf.repaint();
+    public InternalFrameDemo() {
+        setTitle("JInternalFrame Example");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Create a menu bar
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        // Create a menu
+        JMenu fileMenu = new JMenu("File");
+        menuBar.add(fileMenu);
+
+        // Create a menu item to open the internal frame
+        JMenuItem openItem = new JMenuItem("Open Internal Frame");
+        fileMenu.add(openItem);
+
+        // Add action listener to the menu item
+        openItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                createInternalFrame();
+            }
+        });
+
+        // Create a desktop pane to manage internal frames
+        desktopPane = new JDesktopPane();
+        add(desktopPane, BorderLayout.CENTER);
+    }
+
+    private void createInternalFrame() {
+        JInternalFrame internalFrame = new JInternalFrame("Internal Frame", true, true, true, true);
+        internalFrame.setSize(300, 200);
+        internalFrame.setLocation(50, 50);
+
+        // Add some content to the internal frame
+        JLabel label = new JLabel("This is an internal frame");
+        internalFrame.add(label);
+
+        // Make the internal frame visible
+        internalFrame.setVisible(true);
+
+        // Add the internal frame to the desktop pane
+        desktopPane.add(internalFrame);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                InternalFrameDemo mainFrame = new InternalFrameDemo();
+                mainFrame.setVisible(true);
+            }
+        });
     }
 }
